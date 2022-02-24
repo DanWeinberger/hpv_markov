4580bd9a5ae84927c23d43afbd9feaaab6cbf3c7
guinevere-oliver <78618342+guinevere-oliver@users.noreply.github.com>
2022-02-24 12:16:54 -0500

    Treatment success notation change

--- profile_likelihood_age.R
+++ profile_likelihood_age.R
@@ -66,8 +66,8 @@ profile <- function(
   #dcan_dcandeath <- 0.1
   
   # treatment
-  dhsil_norm <- 0.9*0.9
-  dcan_norm <- 0.5*1
+  dhsil_norm <- 0.9*0.9 # % success * % treated
+  dcan_norm <- 0.5*1 # % success (based on ~5 year survival) * % treated
   
   # BRFSS rates!!
   # screening
