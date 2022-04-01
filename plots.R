7b46b619204a623b93a638bebdb7083407c69675
guinevere-oliver <78618342+guinevere-oliver@users.noreply.github.com>
2022-04-01 10:02:49 -0400

    Make plots into grids

--- plots.R
+++ plots.R
@@ -9,6 +9,7 @@
 
 # Call libraries
 library(ggplot2)
+library(gridExtra)
 
 
 #################################### Compare COVID to no COVID ####################################
@@ -58,7 +59,7 @@ p_HSIL<-ggplot(data=result_tot, aes(x=Year)) +
   geom_line(data=result_tot, aes(y=All_HSIL, alpha="Disruption", color="Combined undetected and detected")) +
   coord_cartesian(
     xlim = c(2000,2050),
-    ylim = c(0,750000)) +
+    ylim = c(0,600000)) +
   geom_vline(xintercept = 2008,linetype="dashed",alpha=0.3) +
   geom_vline(xintercept = 2020,linetype="dashed",alpha=0.3) +
   labs(y="HSIL Cases") +
@@ -69,27 +70,43 @@ p_HSIL<-ggplot(data=result_tot, aes(x=Year)) +
 p_HSIL
 
 
-# Cancer Cases and Deaths
+# Cancer Cases
 p_Cancer<-ggplot(data=result_tot, aes(x=Year)) +
   geom_line(data=result_tot_noCov, aes(y=Undet_Cancer,alpha="No disruption"), colour="red") + 
   geom_line(data=result_tot, aes(y=Undet_Cancer,alpha="Disruption",linetype="Undetected"), colour="red") + 
   geom_line(data=result_tot_noCov, aes(y=Det_Cancer,alpha="No disruption"), colour="red") +
   geom_line(data=result_tot, aes(y=Det_Cancer,alpha="Disruption"), colour="red") +
-  geom_line(data=result_tot_noCov, aes(y=Cancer_Death,alpha="No disruption"), colour="red") +
-  geom_line(data=result_tot, aes(y=Cancer_Death,alpha="Disruption",linetype="Cervical Cancer Death"), colour="red") +
-  geom_line(data=result_tot_noCov, aes(y=All_Cancer,alpha="No disruption",color="Combined undetected and detected")) +
-  geom_line(data=result_tot, aes(y=All_Cancer,alpha="Disruption",color="Combined undetected and detected")) +
   coord_cartesian(
     xlim = c(2000,2050),
-    ylim = c(0,55000)) +
+    ylim = c(0,40000)) +
   geom_vline(xintercept = 2008,linetype="dashed",alpha=0.3) +
   geom_vline(xintercept = 2020,linetype="dashed",alpha=0.3) + 
-  labs(y="Cancer Cases/Deaths") +
-  scale_linetype_manual(values=c("Undetected"="dashed", "Detected"="solid","Cervical Cancer Death"="dotted"), name="") +
+  labs(y="Cancer Cases") +
+  scale_linetype_manual(values=c("Undetected"="dashed", "Detected"="solid"), name="") +
   scale_color_manual(values=c("Combined undetected and detected"="black"), name="") +
   scale_alpha_manual(values=c("No disruption"=0.5, "Disruption"=1), name="", guide=guide_legend(override.aes = list(color="red", alpha=c(0.1,1)))) +
   theme_classic()
 p_Cancer
+
+
+# Cancer Deaths
+p_CancerDeath<-ggplot(data=result_tot, aes(x=Year)) +
+  geom_line(data=result_tot_noCov, aes(y=Cancer_Death,alpha="No disruption"), colour="red") +
+  geom_line(data=result_tot, aes(y=Cancer_Death,alpha="Disruption",linetype="Cervical Cancer Death"), colour="red") +
+  coord_cartesian(
+    xlim = c(2000,2050),
+    ylim = c(0,5000)) +
+  geom_vline(xintercept = 2008,linetype="dashed",alpha=0.3) +
+  geom_vline(xintercept = 2020,linetype="dashed",alpha=0.3) + 
+  labs(y="Cancer Deaths") +
+  scale_linetype_manual(values=c("Cervical Cancer Death"="dotted"), name="") +
+  scale_alpha_manual(values=c("No disruption"=0.5, "Disruption"=1), name="", guide=guide_legend(override.aes = list(color="red", alpha=c(0.1,1)))) +
+  theme_classic()
+p_CancerDeath
+
+
+p_all <- grid.arrange(p_HSIL,p_Cancer,p_CancerDeath, ncol=1)
+p_all
 
 
 
@@ -139,30 +156,48 @@ p_HSIL<-ggplot(screen_0.6, aes(x=Year)) +
 p_HSIL
 
 
-# Cancer Cases and Deaths
+# Cancer Cases
 p_Cancer<-ggplot(screen_0.6, aes(x=Year)) +
   geom_line(aes(y=Undet_Cancer, linetype="Undetected"), colour="red") + 
   geom_line(aes(y=Det_Cancer, linetype="Detected"), colour="red") +
-  geom_line(aes(y=Cancer_Death, linetype="Cervical Cancer Death"), colour="red") +
   geom_line(data=screen_0.4, aes(y=Undet_Cancer), colour="red", alpha=0.1) + 
   geom_line(data=screen_0.4, aes(y=Det_Cancer), colour="red", alpha=0.1) +
-  geom_line(data=screen_0.4, aes(y=Cancer_Death), colour="red", alpha=0.1) +
   geom_line(data=screen_0.8, aes(y=Undet_Cancer), colour="red", alpha=0.1) + 
   geom_line(data=screen_0.8, aes(y=Det_Cancer), colour="red", alpha=0.1) +
-  geom_line(data=screen_0.8, aes(y=Cancer_Death), colour="red", alpha=0.1) +
   geom_ribbon(aes(ymin=screen_0.4$Undet_Cancer,ymax=screen_0.8$Undet_Cancer, fill="40%-80% range"), alpha=0.3) +
   geom_ribbon(aes(ymin=screen_0.8$Det_Cancer,ymax=screen_0.4$Det_Cancer, fill="40%-80% range"), alpha=0.3) +
-  geom_ribbon(aes(ymin=screen_0.8$Cancer_Death,ymax=screen_0.4$Cancer_Death, fill="40%-80% range"), alpha=0.3) +
   coord_cartesian(
     xlim = c(2000,2050),
     ylim = c(0,40000)) +
   geom_vline(xintercept = 2008,linetype="dashed",alpha=0.3) +
   geom_vline(xintercept = 2020,linetype="dashed",alpha=0.3) +
-  labs(y="Cancer Cases/Deaths") +
-  scale_linetype_manual(values=c("Undetected"="dashed", "Detected"="solid", "Cervical Cancer Death"="dotted"), name="") +
+  labs(y="Cancer Cases") +
+  scale_linetype_manual(values=c("Undetected"="dashed", "Detected"="solid"), name="") +
   scale_fill_manual(values=c("40%-80% range"="red"),name="",guide=guide_legend(override.aes = list(alpha=c(0.3)))) + 
   theme_classic()
 p_Cancer
+
+
+# Cancer Deaths
+p_CancerDeath<-ggplot(screen_0.6, aes(x=Year)) +
+  geom_line(aes(y=Cancer_Death, linetype="Cervical Cancer Death"), colour="red") +
+  geom_line(data=screen_0.4, aes(y=Cancer_Death), colour="red", alpha=0.1) +
+  geom_line(data=screen_0.8, aes(y=Cancer_Death), colour="red", alpha=0.1) +
+  geom_ribbon(aes(ymin=screen_0.8$Cancer_Death,ymax=screen_0.4$Cancer_Death, fill="40%-80% range"), alpha=0.3) +
+  coord_cartesian(
+    xlim = c(2000,2050),
+    ylim = c(0,5000)) +
+  geom_vline(xintercept = 2008,linetype="dashed",alpha=0.3) +
+  geom_vline(xintercept = 2020,linetype="dashed",alpha=0.3) +
+  labs(y="Cancer Deaths") +
+  scale_linetype_manual(values=c("Cervical Cancer Death"="dotted"), name="") +
+  scale_fill_manual(values=c("40%-80% range"="red"),name="",guide=guide_legend(override.aes = list(alpha=c(0.3)))) + 
+  theme_classic()
+p_CancerDeath
+
+
+p_all <- grid.arrange(p_HSIL,p_Cancer,p_CancerDeath, ncol=1)
+p_all
 
 
 
@@ -195,65 +230,169 @@ p_LSIL_race<-ggplot(Results_white, aes(x=Year)) +
 p_LSIL_race
 
 
-# HSIL
-p_HSIL_race<-ggplot(Results_white, aes(x=Year)) +
+# HSIL (White)
+p_HSIL_white<-ggplot(Results_white, aes(x=Year)) +
   geom_line(data=Results_white, aes(y=Undet_HSIL,linetype="Undetected",color="non-Hispanic White",alpha="Disruption")) + 
   geom_line(data=Results_white, aes(y=Det_HSIL,linetype="Detected",color="non-Hispanic White",alpha="Disruption")) +
-  geom_line(data=Results_black, aes(y=Undet_HSIL,linetype="Undetected",color="non-Hispanic Black",alpha="Disruption")) + 
-  geom_line(data=Results_black, aes(y=Det_HSIL,linetype="Detected",color="non-Hispanic Black",alpha="Disruption")) +
-  geom_line(data=Results_hisp, aes(y=Undet_HSIL,linetype="Undetected",color="Hispanic",alpha="Disruption")) + 
-  geom_line(data=Results_hisp, aes(y=Det_HSIL,linetype="Detected",color="Hispanic",alpha="Disruption")) +
   geom_line(data=Results_white_noCov, aes(y=Undet_HSIL,color="non-Hispanic White",alpha="No disruption")) + 
   geom_line(data=Results_white_noCov, aes(y=Det_HSIL,color="non-Hispanic White",alpha="No disruption")) +
+  coord_cartesian(
+    xlim = c(2000,2050),
+    ylim = c(0,300000)) +
+  geom_vline(xintercept = 2008,linetype="dashed",alpha=0.3) +
+  geom_vline(xintercept = 2020,linetype="dashed",alpha=0.3) +
+  scale_linetype_manual(values=c("Undetected"="dashed", "Detected"="solid"), name="") +
+  scale_color_manual(values=c("non-Hispanic White"="red"), name="") +
+  scale_alpha_manual(values=c("No disruption"=0.5, "Disruption"=1), name="", guide=guide_legend(override.aes = list(color="black", alpha=c(0.05,1)))) +
+  labs(y="HSIL Cases") +
+  theme_classic()
+p_HSIL_white
+
+# HSIL (Black)
+p_HSIL_black<-ggplot(Results_black, aes(x=Year)) +
+  geom_line(data=Results_black, aes(y=Undet_HSIL,linetype="Undetected",color="non-Hispanic Black",alpha="Disruption")) + 
+  geom_line(data=Results_black, aes(y=Det_HSIL,linetype="Detected",color="non-Hispanic Black",alpha="Disruption")) +
   geom_line(data=Results_black_noCov, aes(y=Undet_HSIL,color="non-Hispanic Black",alpha="No disruption")) + 
   geom_line(data=Results_black_noCov, aes(y=Det_HSIL,color="non-Hispanic Black",alpha="No disruption")) +
+  coord_cartesian(
+    xlim = c(2000,2050),
+    ylim = c(0,50000)) +
+  geom_vline(xintercept = 2008,linetype="dashed",alpha=0.3) +
+  geom_vline(xintercept = 2020,linetype="dashed",alpha=0.3) +
+  scale_linetype_manual(values=c("Undetected"="dashed", "Detected"="solid"), name="") +
+  scale_color_manual(values=c("non-Hispanic Black"="blue"), name="") +
+  scale_alpha_manual(values=c("No disruption"=0.5, "Disruption"=1), name="", guide=guide_legend(override.aes = list(color="black", alpha=c(0.05,1)))) +
+  labs(y="HSIL Cases") +
+  theme_classic()
+p_HSIL_black
+
+# HSIL (Hispanic)
+p_HSIL_hisp<-ggplot(Results_hisp, aes(x=Year)) +
+  geom_line(data=Results_hisp, aes(y=Undet_HSIL,linetype="Undetected",color="Hispanic",alpha="Disruption")) + 
+  geom_line(data=Results_hisp, aes(y=Det_HSIL,linetype="Detected",color="Hispanic",alpha="Disruption")) +
   geom_line(data=Results_hisp_noCov, aes(y=Undet_HSIL,color="Hispanic",alpha="No disruption")) + 
   geom_line(data=Results_hisp_noCov, aes(y=Det_HSIL,color="Hispanic",alpha="No disruption")) +
   coord_cartesian(
     xlim = c(2000,2050),
-    ylim = c(0,200000)) +
+    ylim = c(0,70000)) +
   geom_vline(xintercept = 2008,linetype="dashed",alpha=0.3) +
   geom_vline(xintercept = 2020,linetype="dashed",alpha=0.3) +
   scale_linetype_manual(values=c("Undetected"="dashed", "Detected"="solid"), name="") +
-  scale_color_manual(values=c("non-Hispanic White"="red","non-Hispanic Black"="blue","Hispanic"="green"), name="") +
+  scale_color_manual(values=c("Hispanic"="green"), name="") +
   scale_alpha_manual(values=c("No disruption"=0.5, "Disruption"=1), name="", guide=guide_legend(override.aes = list(color="black", alpha=c(0.05,1)))) +
   labs(y="HSIL Cases") +
   theme_classic()
-p_HSIL_race
+p_HSIL_hisp
 
 
-# Cancer Cases and Deaths
-p_Cancer_race<-ggplot(Results_white, aes(x=Year)) +
+# Cancer Cases (White)
+p_Cancer_white<-ggplot(Results_white, aes(x=Year)) +
   geom_line(data=Results_white, aes(y=Undet_Cancer,color="non-Hispanic White",linetype="Undetected",alpha="Disruption")) + 
   geom_line(data=Results_white, aes(y=Det_Cancer,color="non-Hispanic White",linetype="Detected",alpha="Disruption")) +
-  geom_line(data=Results_white, aes(y=Cancer_Death,color="non-Hispanic White",linetype="Cervical Cancer Death",alpha="Disruption")) +
-  geom_line(data=Results_black, aes(y=Undet_Cancer,color="non-Hispanic Black",linetype="Undetected",alpha="Disruption")) + 
-  geom_line(data=Results_black, aes(y=Det_Cancer,color="non-Hispanic Black",linetype="Detected",alpha="Disruption")) +
-  geom_line(data=Results_black, aes(y=Cancer_Death,color="non-Hispanic Black",linetype="Cervical Cancer Death",alpha="Disruption")) +
-  geom_line(data=Results_hisp, aes(y=Undet_Cancer,color="Hispanic",linetype="Undetected",alpha="Disruption")) + 
-  geom_line(data=Results_hisp, aes(y=Det_Cancer,color="Hispanic",linetype="Detected",alpha="Disruption")) +
-  geom_line(data=Results_hisp, aes(y=Cancer_Death,color="Hispanic",linetype="Cervical Cancer Death",alpha="Disruption")) +
   geom_line(data=Results_white_noCov, aes(y=Undet_Cancer,color="non-Hispanic White",alpha="No disruption")) + 
   geom_line(data=Results_white_noCov, aes(y=Det_Cancer,color="non-Hispanic White",alpha="No disruption")) +
-  geom_line(data=Results_white_noCov, aes(y=Cancer_Death,color="non-Hispanic White",alpha="No disruption")) +
-  geom_line(data=Results_black_noCov, aes(y=Undet_Cancer,color="non-Hispanic Black",alpha="No disruption")) + 
-  geom_line(data=Results_black_noCov, aes(y=Det_Cancer,color="non-Hispanic Black",alpha="No disruption")) +
-  geom_line(data=Results_black_noCov, aes(y=Cancer_Death,color="non-Hispanic Black",alpha="No disruption")) +
-  geom_line(data=Results_hisp_noCov, aes(y=Undet_Cancer,color="Hispanic",alpha="No disruption")) + 
-  geom_line(data=Results_hisp_noCov, aes(y=Det_Cancer,color="Hispanic",alpha="No disruption")) +
-  geom_line(data=Results_hisp_noCov, aes(y=Cancer_Death,color="Hispanic",alpha="No disruption")) +
   coord_cartesian(
     xlim = c(2000,2050),
     ylim = c(0,30000)) +
   geom_vline(xintercept = 2008,linetype="dashed",alpha=0.3) +
   geom_vline(xintercept = 2020,linetype="dashed",alpha=0.3) +
-  scale_linetype_manual(values=c("Undetected"="dashed", "Detected"="solid","Cervical Cancer Death"="dotted"), name="") +
-  scale_color_manual(values=c("non-Hispanic White"="red","non-Hispanic Black"="blue","Hispanic"="green"), name="") +
+  scale_linetype_manual(values=c("Undetected"="dashed", "Detected"="solid"), name="") +
+  scale_color_manual(values=c("non-Hispanic White"="red"), name="") +
   scale_alpha_manual(values=c("No disruption"=0.5, "Disruption"=1), name="", guide=guide_legend(override.aes = list(color="black", alpha=c(0.03,1)))) +
-  labs(y="Cancer Cases/Deaths") +
+  labs(y="Cancer Cases") +
   theme_classic()
-p_Cancer_race
+p_Cancer_white
 
 
+# Cancer Deaths (White)
+p_CancerDeath_white<-ggplot(Results_white, aes(x=Year)) +
+  geom_line(data=Results_white, aes(y=Cancer_Death,color="non-Hispanic White",linetype="Cervical Cancer Death",alpha="Disruption")) +
+  geom_line(data=Results_white_noCov, aes(y=Cancer_Death,color="non-Hispanic White",alpha="No disruption")) +
+  coord_cartesian(
+    xlim = c(2000,2050),
+    ylim = c(0,4000)) +
+  geom_vline(xintercept = 2008,linetype="dashed",alpha=0.3) +
+  geom_vline(xintercept = 2020,linetype="dashed",alpha=0.3) +
+  scale_linetype_manual(values=c("Cervical Cancer Death"="dotted"), name="") +
+  scale_color_manual(values=c("non-Hispanic White"="red"), name="") +
+  scale_alpha_manual(values=c("No disruption"=0.5, "Disruption"=1), name="", guide=guide_legend(override.aes = list(color="black", alpha=c(0.03,1)))) +
+  labs(y="Cancer Deaths") +
+  theme_classic()
+p_CancerDeath_white
 
+
+# Cancer Cases (Black)
+p_Cancer_black<-ggplot(Results_black, aes(x=Year)) +
+  geom_line(data=Results_black, aes(y=Undet_Cancer,color="non-Hispanic Black",linetype="Undetected",alpha="Disruption")) + 
+  geom_line(data=Results_black, aes(y=Det_Cancer,color="non-Hispanic Black",linetype="Detected",alpha="Disruption")) +
+  geom_line(data=Results_black_noCov, aes(y=Undet_Cancer,color="non-Hispanic Black",alpha="No disruption")) + 
+  geom_line(data=Results_black_noCov, aes(y=Det_Cancer,color="non-Hispanic Black",alpha="No disruption")) +
+  coord_cartesian(
+    xlim = c(2000,2050),
+    ylim = c(0,5000)) +
+  geom_vline(xintercept = 2008,linetype="dashed",alpha=0.3) +
+  geom_vline(xintercept = 2020,linetype="dashed",alpha=0.3) +
+  scale_linetype_manual(values=c("Undetected"="dashed", "Detected"="solid"), name="") +
+  scale_color_manual(values=c("non-Hispanic Black"="blue"), name="") +
+  scale_alpha_manual(values=c("No disruption"=0.5, "Disruption"=1), name="", guide=guide_legend(override.aes = list(color="black", alpha=c(0.03,1)))) +
+  labs(y="Cancer Cases") +
+  theme_classic()
+p_Cancer_black
+
+
+# Cancer Deaths (Black)
+p_CancerDeath_black<-ggplot(Results_black, aes(x=Year)) +
+  geom_line(data=Results_black, aes(y=Cancer_Death,color="non-Hispanic Black",linetype="Cervical Cancer Death",alpha="Disruption")) +
+  geom_line(data=Results_black_noCov, aes(y=Cancer_Death,color="non-Hispanic Black",alpha="No disruption")) +
+  coord_cartesian(
+    xlim = c(2000,2050),
+    ylim = c(0,600)) +
+  geom_vline(xintercept = 2008,linetype="dashed",alpha=0.3) +
+  geom_vline(xintercept = 2020,linetype="dashed",alpha=0.3) +
+  scale_linetype_manual(values=c("Cervical Cancer Death"="dotted"), name="") +
+  scale_color_manual(values=c("non-Hispanic Black"="blue"), name="") +
+  scale_alpha_manual(values=c("No disruption"=0.5, "Disruption"=1), name="", guide=guide_legend(override.aes = list(color="black", alpha=c(0.03,1)))) +
+  labs(y="Cancer Deaths") +
+  theme_classic()
+p_CancerDeath_black
+
+
+# Cancer Cases (Hisp)
+p_Cancer_hisp<-ggplot(Results_hisp, aes(x=Year)) +
+  geom_line(data=Results_hisp, aes(y=Undet_Cancer,color="Hispanic",linetype="Undetected",alpha="Disruption")) + 
+  geom_line(data=Results_hisp, aes(y=Det_Cancer,color="Hispanic",linetype="Detected",alpha="Disruption")) +
+  geom_line(data=Results_hisp_noCov, aes(y=Undet_Cancer,color="Hispanic",alpha="No disruption")) + 
+  geom_line(data=Results_hisp_noCov, aes(y=Det_Cancer,color="Hispanic",alpha="No disruption")) +
+  coord_cartesian(
+    xlim = c(2000,2050),
+    ylim = c(0,10000)) +
+  geom_vline(xintercept = 2008,linetype="dashed",alpha=0.3) +
+  geom_vline(xintercept = 2020,linetype="dashed",alpha=0.3) +
+  scale_linetype_manual(values=c("Undetected"="dashed", "Detected"="solid"), name="") +
+  scale_color_manual(values=c("Hispanic"="green"), name="") +
+  scale_alpha_manual(values=c("No disruption"=0.5, "Disruption"=1), name="", guide=guide_legend(override.aes = list(color="black", alpha=c(0.03,1)))) +
+  labs(y="Cancer Cases") +
+  theme_classic()
+p_Cancer_hisp
+
+
+# Cancer Deaths (Hisp)
+p_CancerDeath_hisp<-ggplot(Results_hisp, aes(x=Year)) +
+  geom_line(data=Results_hisp, aes(y=Cancer_Death,color="Hispanic",linetype="Cervical Cancer Death",alpha="Disruption")) +
+  geom_line(data=Results_hisp_noCov, aes(y=Cancer_Death,color="Hispanic",alpha="No disruption")) +
+  coord_cartesian(
+    xlim = c(2000,2050),
+    ylim = c(0,1000)) +
+  geom_vline(xintercept = 2008,linetype="dashed",alpha=0.3) +
+  geom_vline(xintercept = 2020,linetype="dashed",alpha=0.3) +
+  scale_linetype_manual(values=c("Cervical Cancer Death"="dotted"), name="") +
+  scale_color_manual(values=c("Hispanic"="green"), name="") +
+  scale_alpha_manual(values=c("No disruption"=0.5, "Disruption"=1), name="", guide=guide_legend(override.aes = list(color="black", alpha=c(0.03,1)))) +
+  labs(y="Cancer Deaths") +
+  theme_classic()
+p_CancerDeath_hisp
+
+
+p_race <- grid.arrange(p_HSIL_white,p_HSIL_black,p_HSIL_hisp,p_Cancer_white,p_Cancer_black,p_Cancer_hisp,p_CancerDeath_white,p_CancerDeath_black,p_CancerDeath_hisp, ncol=3, nrow=3)
+p_race
 
